21d26b7f4f199fb52e1d7ff60342ec101fb67df7
Jean <jeanportugalcadastrar@gmail.com>
2024-03-22 22:59:15 +0000

    :toda: started project

---
+++ prj-first-api/prj-first-api/src/main/java/com/jps/firstapi/PrjFirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.jps.firstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjFirstApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjFirstApiApplication.class, args);
+    }
+
+}
---
+++ prj-first-api/prj-first-api/src/test/java/com/jps/firstapi/PrjFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jps.firstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjFirstApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
