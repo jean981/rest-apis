684bf701cdcc7cd353deef6f20c452be666fc121
Jean <jeanportugalcadastrar@gmail.com>
2024-03-24 03:48:31 +0000

    :sparkles: feat(StandardError): created customized exception

--- prj-first-api/src/main/java/com/jps/firstapi/services/UserServiceImp.java
+++ prj-first-api/src/main/java/com/jps/firstapi/services/UserServiceImp.java
@@ -2,6 +2,7 @@ package com.jps.firstapi.services;
 
 import com.jps.firstapi.domain.User;
 import com.jps.firstapi.repositories.UserRepository;
+import com.jps.firstapi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class UserServiceImp implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> user = userRepository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(()-> new ObjectNotFoundException("The object not found."));
     }
 }
