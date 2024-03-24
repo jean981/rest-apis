1c5ad13bd1f67d44ba19267842d5fb2e63cc4fee
Jean <jeanportugalcadastrar@gmail.com>
2024-03-24 00:33:35 +0000

    :sparkles: feat(UserRepository): created interface

---
+++ prj-first-api/src/main/java/com/jps/firstapi/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jps.firstapi.repositories;
+
+import com.jps.firstapi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
